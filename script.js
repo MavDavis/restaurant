69aeb08295429b2040fa395109f86a6cd68c7358
MavDavis <ekwesimarvelous@gmail.com>
2022-06-01 23:18:06 +0100

    commit

--- script.js
+++ script.js
@@ -1,73 +1,129 @@
- let menu = document.querySelector('#menu-bars') ;
- let navbar = document.querySelector('.navbar') 
-
- menu.onclick = () => {
-     menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
- }
- let section = document.querySelectorAll('section');
- let navLinks = document.querySelectorAll('header .navbar a');
-
- window.onscroll = () => {
-     menu.classList.remove('fa-times');
-     navbar.classList.remove('active');
-
- section.forEach(sec =>{
-let top = window.scrollY;
-let height = sec.offsetHeight;
-let offset = sec.offsetTop - 150;
-let id = sec.getAttribute('id');
-
-if(top => offset && top < offset + height){
-   navLinks.forEach( links =>{
-      links.classList.remove('active');
-      console.log(document.querySelector('header .navbar a[href*='+id+']'))
-      document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
-
-   });
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+let boxContainer = document.querySelector(".box-container");
+let buttonsDom = [];
+let cart = [];
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
 };
-     });
- }
- document.querySelector('#search-icon').onclick = () =>{
-    document.querySelector('#search-form').classList.toggle('active');
- }
- document.querySelector('#close').onclick = () =>{
-    document.querySelector('#search-form').classList.remove('active');
- }
- var swiper = new Swiper(".home-slider",{
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay:{
-       delay:7500,
-       disableOnInteraction: false,
-    },
-    pagination:{
-       el:".swiper-pagination",
-       clickable: true,
-    },
-   loop:true,
- });
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header .navbar a");
 
- var swiper = new Swiper(".review-slider",{
-   spaceBetween: 20,
-   centeredSlides: true,
-   autoplay: {
-      delay:7500,
-      disableOnInteraction: false,
-   },
-  loop:true,
-  breakpoints: {
-  0: {
-     slidesPerView: 1
-  },
-  640: {
-   slidesPerView: 2
-},
-768: {
-   slidesPerView: 2
-},
-1024: {
-   slidesPerView: 3
-  },
- },
+class FetchProduct {
+  async getProducts() {
+    try {
+      let food = await fetch("products.json");
+      let toJson = await food.json();
+      let dishes = toJson.dishes;
+      return dishes;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+class Cart {
+  createDishDiv(dishes) {
+    let box = "";
+    for (var i = 0; i < dishes.length; i++) {
+      let img = dishes[i].img;
+      let name = dishes[i].name;
+      let price = dishes[i].price;
+      let id = dishes[i].id;
+
+      box += `<div class="box">
+                <a href="#"><i class="fa fa-heart"></i></a>
+                <a href="#"><i class="fa fa-eye"></i></a>
+                <img src=${img}alt="product${name}">
+                <h3>${name}</h3>
+                <div class="stars">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-alt"></i>
+                </div>
+                <span><i class='fa fa-&#8358'>&#8358;</i> ${price}</span>
+                <button class="btn orderbtn" data-id=${id}>order here</button>
+            </div>`;
+    }
+    boxContainer.innerHTML = box;
+  }
+
+  storeDishes(item) {
+    localStorage.setItem("item", JSON.stringify(item));
+  }
+  setCartToStorage(item) {
+    if (localStorage.key("cartitem") == "") {
+      return;
+    } else {
+      localStorage.setItem("cartitem");
+    }
+  }
+  getCartFromLocaStorage() {
+    if (localStorage.key("cartitem") != "") {
+      let fetchedItem = JSON.parse(localStorage.getItem("cartitem"));
+      this.updateDisplay(fetchedItem);
+      const buttons2 = [...document.querySelectorAll(".orderbtn")];
+      buttons2.forEach((btn) => {});
+    }
+  }
+  updateDisplay(fetchedItem) {
+    let CartDiv = document.getElementById("cartDiv");
+    let user = "";
+    fetchedItem.forEach((displayCart) => {
+      user += `
+              <div class="item">
+            <div class="img">
+                <img src=${displayCart.img} alt="">
+            </div>
+            <div class="details">
+                <p class="name">${displayCart.names}</p>
+                <p class="price">${displayCart.price}</p>
+            </div>
+            <div class="remove-btn btn">remove</div>
+        </div>
+       
+            `;
+    });
+    CartDiv.innerHTML = user;
+  }
+  callDishes(dishes) {
+    this.createDishDiv(dishes);
+    this.storeDishes(dishes);
+  }
+  getOrderBtn() {
+    const buttons = [...document.querySelectorAll(".orderbtn")];
+    buttonsDom = buttons;
+    buttons.forEach((btn) => {
+      btn.addEventListener("click", (event) => {
+        let id = btn.dataset.id;
+        event.target.innerText = "In Cart";
+        event.target.disabled = true;
+        let fetchProductForCart = new FetchProduct();
+        let cartProducts = fetchProductForCart
+          .getProducts()
+          .then((dishes) => this.CartCallDishes(id, dishes));
+      });
+    });
+  }
+  CartCallDishes(id, dishes) {
+    let perCartItem = dishes.find((perCart) => perCart.id == id);
+    cart = [...cart, perCartItem];
+    this.setCartToStorage(cart);
+    this.getCartFromLocaStorage();
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  let cart = new Cart();
+  let fetchProduct = new FetchProduct();
+  fetchProduct
+    .getProducts()
+    .then((dishes) => cart.callDishes(dishes))
+    .then(() => {
+      cart.getOrderBtn();
+      cart.getCartFromLocaStorage();
+    });
 });
